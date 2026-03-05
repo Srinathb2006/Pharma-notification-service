f16f407973f59939d61ac808de58245049a8cad4
Srinathb2006 <133136966+Srinathb2006@users.noreply.github.com>
2026-03-05 18:31:58 +0530

    Update EmailController.java

--- src/main/java/com/kce/pharma/controller/EmailController.java
+++ src/main/java/com/kce/pharma/controller/EmailController.java
@@ -1,22 +1,26 @@
 package com.kce.pharma.controller;
 
-import com.kce.pharma.dto.EmailRequest;
 import org.springframework.web.bind.annotation.*;
-
 import com.kce.pharma.service.EmailService;
 
 @RestController
 @RequestMapping("/email")
 public class EmailController {
 
-    private final EmailService emailService;
 
-    public EmailController(EmailService emailService) {
-        this.emailService = emailService;
-    }
+private final EmailService emailService;
 
-    @PostMapping("/send")
-    public void send(@RequestBody EmailRequest request) {
-        emailService.send(request.getTo(), request.getSubject(), request.getBody());
-    }
+public EmailController(EmailService emailService) {
+    this.emailService = emailService;
+}
+
+@PostMapping("/send")
+public void send(
+        @RequestParam String to,
+        @RequestParam String subject,
+        @RequestParam String body
+) {
+    emailService.send(to, subject, body);
+}
+
 }
